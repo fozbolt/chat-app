8ef6c88a9183a8920fbc1c04ad1541685483b048
filipozbolt <filip.ozbolt@preskok.si>
2024-02-10 11:31:08 +0100

    improved app port conection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as portfinder from 'portfinder';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000); //.env or in ConfigKeys
+    const desiredPort = parseInt(process.env.PORT) || 3001; //add in configKeys?
+
+    try {
+        const port = await portfinder.getPortPromise({ port: desiredPort });
+        const app = await NestFactory.create(AppModule);
+        await app.listen(port);
+        console.log(`Application is running on port ${port}`);
+    } catch (err) {
+        console.error('Error finding an available port:', err);
+    }
 }
+
 bootstrap();
