66e8a144453a6edbafd2ae05104be8e1fc04c111
filipozbolt <filip.ozbolt@preskok.si>
2024-05-11 11:21:49 +0200

    linters and formatters update

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 // other options are removed from explicit assignment since they are used by default
 module.exports = {
-  printWidth: 160,
-  singleQuote: true,
-  tabWidth: 4,
+    printWidth: 120,
+    singleQuote: true,
+    tabWidth: 4,
 };
